19fcd9f0d29199bfdf476b7c3e21964d6522a6d8
Kaan Er <erkaan1@outlook.com>
2020-10-23 21:47:58 +0300

    with python

---
+++ beautiful_days_at_the_movies.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulDays function below.
+def beautifulDays(i, j, k):
+    counter=0
+    for l in range(i, j+1):
+        a = (str(l)[::-1])
+        if(abs(l-int(a))%k == 0):
+            counter+=1
+    return counter
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ijk = input().split()
+
+    i = int(ijk[0])
+
+    j = int(ijk[1])
+
+    k = int(ijk[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
